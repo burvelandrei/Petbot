dc0d3af1c3487ef4707c712ef951e2ea16e9d017
Andrei Burvel <burvelandrei@gmail.com>
2024-04-24 23:53:44 +0300

    дообнова

--- logging_data/logging_settings.py
+++ logging_data/logging_settings.py
@@ -1,1 +1,44 @@
 import logging
+
+
+# Фильт на ошибки Error или Critical
+class ErrorLogFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord):
+        return record.levelname in ("ERROR", "CRITICAL")
+
+
+# Словарь настроек логгирования
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "formatter_1": {
+            "format": "%(filename)s:%(lineno)d #%(levelname)-8s "
+            "[%(asctime)s] - %(name)s - %(message)s"
+        },
+    },
+    "filters": {
+        "error_filter": {
+            "()": ErrorLogFilter,
+        }
+    },
+    "handlers": {
+        "all": {
+            "class": "logging.StreamHandler",
+            "formatter": "formatter_1",
+        },
+        "error_file": {
+            "class": "logging.FileHandler",
+            "filename": "ERROR.txt",
+            "mode": "a",
+            "level": "DEBUG",
+            "formatter": "formatter_1",
+            "filters": ["error_filter"],
+        },
+    },
+    "root": {
+        "level": "DEBUG",
+        "formatter": "formatter_1",
+        "handlers": ["all", "error_file"],
+    },
+}
